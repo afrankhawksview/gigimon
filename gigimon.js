f0da524eda51ad3448ed69c8f6c035019b7bf01a
afrankhawksview <afrank@hawksviewhomes.com>
2015-06-18 09:26:24 -0400

    Basic Set Up
    jQuery pokemon style game with two combatants and one action, 'attack'

---
+++ gigimon.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+ var gigi = {
+ 	hp:100,
+ 	attack:10
+ };
+
+ var pikachu = {
+ 	hp:90,
+ 	attack:10
+ };
+
+ function attack(attacker,defender){
+
+ }
+
+ $('.attack').click(function(){
+ 	$('#log').html("<p>Attack!</p>");
+ });
+
+});
---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title>Gigimon Battle</title>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="gigimon.js"></script>
+</head>
+<body>
+	<div id="log">
+	</div>
+	<p class="attack">Attack</p>
+
+</body>
+</html>
